82167703960948d657e3421428b83abdfa38a882
Giovani Patrick <=>
2022-03-05 15:26:57 -0300

    Criando Layout - Tela de Login

--- App/Resources/Views/index.php
+++ App/Resources/Views/index.php
@@ -6,46 +6,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frente de Caixa - Project</title>
     <link rel="stylesheet" href="App/Resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="App/Resources/css/all.min.css">
     <script src="App/Resources/js/bootstrap.bundle.min.js"></script>
     <style>
+
+        body{
+            background-image:url('App/Resources/images/pdv_bg.png'); 
+            background-size: cover; 
+            background-repeat:repeat-y; 
+            background-color:#2a9df4;
+        }
+
         body,.input-group-text,.btn,.form-control{
             font-family: sans-serif !important;
             font-size: 15px !important;
         }
+
+        .input-group-text{
+            width:45px;
+        }
+
+        .span-login, .btn-login{
+            text-shadow: 1px 1px black; 
+            background-color:#2a9df4;
+        }
+
+        .div-login{
+            max-width: 30%; 
+            margin-top:11%; 
+            background-color:#C0C0C0cc;
+        }
+
+        .a-cad{
+            font-weight:bold; 
+            text-decoration:none; 
+            text-shadow: 1px 1px silver;
+        }
     </style>
 </head>
-<body style="background-image:url('App/Resources/images/pdv_bg.png'); background-size: cover; background-repeat:repeat-y">
+<body>
 
-<div class="mx-auto text-center shadow-lg p-5 border border-secondary rounded" style="max-width: 30%; margin-top:11%;">
+<div class="div-login mx-auto text-center shadow-lg p-4 border border-secondary rounded">
 
-<section>
-    <h5 class="p-2 text-light" style="text-shadow: 1px 1px black;">Frente de Caixa - Project</h5>
-</section>
+<div id="logo">
+    <img src="App/Resources/images/pdv_logo.png" class="img-fluid">
+</div>
 
 <div class="input-group mt-4">
-    <div class="input-group-prepend" style="width:28%;">
-        <span class="input-group-text p-2 bg-secondary bg-gradient border-0 text-light rounded shadow" style="text-shadow: 1px 1px black;">
-            Usuário
+    <div class="input-group-prepend text-center">
+        <span class="input-group-text span-login p-2 border-0 rounded shadow">
+        <i class="fa-solid fa-user fa-2x text-light" aria-hidden="true"></i>
         </span>
     </div>
     <input type="text" class="form-control" placeholder="Digite o usuário..."/>
 </div>
 
 <div class="input-group mt-1">
-    <div class="input-group-prepend" style="width:28%;">
-        <span class="input-group-text p-2 bg-secondary bg-gradient border-0 text-light rounded shadow" style="text-shadow: 1px 1px black;">
-            Senha  
+    <div class="input-group-prepend text-center">
+        <span class="input-group-text span-login p-2 border-0 rounded shadow">
+        <i class="fa-solid fa-key fa-2x text-light" aria-hidden="true"></i>
         </span>
     </div>
     <input type="text" class="form-control" placeholder="Digite a senha..."/>
 </div>
 
-<button type="submit" class="mt-2 p-2 btn btn-secondary bg-gradient rounded shadow text-light" style="text-shadow: 1px 1px black;">
+<button type="submit" class="fw-bold btn-login mt-2 p-2 btn rounded shadow text-light">
 Autenticar
 </button>
 
 <hr>
-<a class="text-light" href="#" style="text-decoration:none; text-shadow: 1px 1px black;">Não possui conta? <br>Cadastre-se agora!</a>
+<a href="#" class="a-cad">Não possui conta? <br>Cadastre-se agora!</a>
 
 </div>
 
